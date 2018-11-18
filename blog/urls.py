e3e52d0c07c4f3362d7fc33cea8e3d82140632c5
freshairnugget <freshairnugget@gmail.com>
2018-11-18 15:28:23 +0700

    added views and urls

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,re_path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('',views.PostListView.as_view(),name='post_list'),
+    path('about/',views.AboutView.as_view(),name='about'),
+    path('post/<int:pk>',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.PostCreateView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit/',views.PostUpdateView.as_view(),name='post_update'),
+    path('post/<int:pk>/remove/',views.PostDeleteView.as_view(),name='post_remove'),
+    path('drafts/',views.DraftListView.as_view(),name='draft'),
+]
